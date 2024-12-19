545bdb0ebee044822329e0be1dfe78782e4b13f2
AndreiSager <andreisager@proton.me>
2024-12-19 15:35:34 +0800

    chore: downgrade to react 18

---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+const button = cva("button", {
+  variants: {
+    intent: {
+      primary: [
+        "bg-white",
+        "text-black",
+        "border-transparent",
+        "hover:bg-slate-100",
+        "rounded-lg",
+      ],
+      secondary: [
+        "bg-[#23252c]",
+        "text-white",
+        "border-transparent",
+        "rounded-lg",
+      ],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {}
+
+const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  ...props
+}) => <button className={button({ intent, size, className })} {...props} />;
+
+export default Button;
