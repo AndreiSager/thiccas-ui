8369696a9c466280c5529eeded66f23a4b3b3c20
AndreiSager <andreisager@proton.me>
2024-12-19 16:42:01 +0800

    feat: create avater

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Avatar from "./Avatar";
+
+const meta = {
+  title: "Avatar",
+  component: Avatar,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Circle: Story = {
+  args: {
+    intent: "circle",
+    size: "md",
+    content: "image",
+  },
+};
+
+export const Square: Story = {
+  args: {
+    intent: "square",
+    size: "md",
+    content: "image",
+  },
+};
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,38 @@
+import * as RAvatar from "@radix-ui/react-avatar";
+import { cva } from "class-variance-authority";
+
+const avatar = cva("avatar", {
+  variants: {
+    intent: {
+      circle: ["rounded-full"],
+      square: ["rounded-sm"],
+    },
+    size: {
+      md: ["text-base"],
+    },
+  },
+  defaultVariants: {
+    intent: "circle",
+    size: "md",
+  },
+});
+
+const Avatar = ({
+  intent,
+  size,
+  content,
+  ...props
+}: {
+  intent?: "circle" | "square";
+  size: "3xs" | "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+  content: "image" | "letter" | "smile";
+}) => {
+  return (
+    <RAvatar.Root>
+      <RAvatar.Image />
+      <RAvatar.Fallback />
+    </RAvatar.Root>
+  );
+};
+
+export default Avatar;
