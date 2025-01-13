bfa89d9922e7939d3a4665288330048be6e568b0
AndreiSager <andreisager@proton.me>
2025-01-13 13:03:47 +0800

    refactor(avatar): change shorthands to whole words

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,39 +1,43 @@
 import * as RAvatar from "@radix-ui/react-avatar";
 import { cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
 
-const avatar = cva("avatar", {
+const avatar = cva(["overflow-hidden select-none"], {
   variants: {
     intent: {
       circle: ["rounded-full"],
       square: ["rounded-sm"],
     },
     size: {
-      md: ["text-base"],
+      small: ["text-sm w-10 h-10"],
+      medium: ["text-base w-12 h-12"],
+      large: ["text-lg"],
     },
   },
   defaultVariants: {
     intent: "circle",
-    size: "md",
+    size: "medium",
   },
 });
 
 const Avatar = ({
-  intent,
-  size,
-  content,
+  intent = "circle",
+  size = "medium",
   ...props
 }: {
   intent?: "circle" | "square";
-  /**
-   * `3xs` `2xs`
-   */
-  size: "3xs" | "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
-  content: "image" | "letter" | "smile";
+  size?: "small" | "medium" | "large";
 }) => {
   return (
-    <RAvatar.Root>
-      <RAvatar.Image />
-      <RAvatar.Fallback />
+    <RAvatar.Root className={twMerge(intent, size)} {...props}>
+      <RAvatar.Image
+        className="AvatarImage"
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        alt="Colm Tuite"
+      />
+      <RAvatar.Fallback className="AvatarFallback" delayMs={600}>
+        CT
+      </RAvatar.Fallback>
     </RAvatar.Root>
   );
 };
